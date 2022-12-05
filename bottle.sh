38131b112f304689f0e2b2a652da57e30239dc4a
celsius narhwal <hello@celsiusnarhwal.dev>
2022-12-05 13:34:28 -0500

    bottle test

--- bottle.sh
+++ bottle.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 formulae=$1
-
+root_url="https://ghcr.io/v2/celsiusnarhwal/htt"
 
 # Tap HTT
 brew tap celsiusnarhwal/htt
@@ -14,25 +14,32 @@ systemctl start docker
 echo "$GITHUB_TOKEN" | docker login ghcr.io -u celsiusnarhwal --password-stdin
 
 for formula in $formulae; do
+  formula_name="$(basename "$formula" .rb)"
   # Bottle formula
   brew install --build-bottle "$(basename "$formula" .rb)"
   brew bottle --json "$(basename "$formula" .rb)"
 
   # Merge DSL
   json_file=$(find . -name "*.json")
-  brew bottle --merge --write "$json_file" --root-url="https://ghcr.io/v2/celsiusnarhwal/homebrew-htt"
-done
+  brew bottle --merge --write "$json_file" --root-url="$root_url"
 
-bottles=$(find . -name "*.tar.gz")
-for bottle in $bottles; do
-  sha256=$(docker import "$bottle")
-  docker tag "$sha256" ghcr.io/celsiusnarhwal/homebrew-htt:"$(basename "$bottle" .tar.gz)"
-  docker push ghcr.io/celsiusnarhwal/homebrew-htt:"$(basename "$bottle" .tar.gz)"
+  bottle=$(find . -name "$formula_name*.tar.gz")
+  version=$(echo "$bottle" | sed -E "s/.*$formula_name-([0-9.]+)\.tar\.gz/\1/")
+  sha256=$(docker import "$bottle" | sed -E "s/.*sha256:(.*)/\1/")
+  docker tag "$sha256" "$root_url/$formula_name:$version@sha256:$sha256"
+  docker push "$root_url/$formula_name:$version@sha256:$sha256"
 done
+#
+#bottles=$(find . -name "*.tar.gz")
+#for bottle in $bottles; do
+#  name="$(grep "")"
+#  sha256=$(docker import "$bottle")
+#  docker tag "$sha256" ghcr.io/celsiusnarhwal/homebrew-htt:"$(basename "$bottle" .tar.gz)"
+#  docker push ghcr.io/celsiusnarhwal/homebrew-htt:"$(basename "$bottle" .tar.gz)"
+#done
 
 
 # Commit and push changes
 cd "$(brew --prefix)"/Homebrew/Library/Taps/celsiusnarhwal/homebrew-htt || exit
-git add -A && git commit -m "Update bottles"
 git pull --merge
 git push
