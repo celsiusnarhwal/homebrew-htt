9e84d7608dff93360128cc11b7bb22f722d7eff3
celsius narhwal <hello@celsiusnarhwal.dev>
2022-12-07 21:50:57 -0500

    bottle test

--- bottle.sh
+++ bottle.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 formula=$1
-formula_name="$(basename "$formula" .rb)"
-root_url="https://ghcr.io/v2/celsiusnarhwal/htt"
 
 # Tap HTT
 brew tap celsiusnarhwal/htt
@@ -11,17 +9,10 @@ brew tap celsiusnarhwal/htt
 brew install --build-bottle "$(basename "$formula" .rb)"
 brew bottle --json "$(basename "$formula" .rb)"
 
-# Merge DSL
-json_file=$(find . -name "*.json")
-brew bottle --merge --write "$json_file" --root-url="$root_url"
+# Upload bottle
+brew pr-upload --root-url="https://ghcr.io/v2/celsiusnarhwal/htt"
 
-bottle=$(find . -name "$formula_name*.tar.gz")
-version=$(echo "$bottle" | sed -E "s/.*$formula_name-([0-9.]+)\.tar\.gz/\1/")
-sha256=$(docker import "$bottle" | sed -E "s/.*sha256:(.*)/\1/")
-docker tag "$sha256" "$root_url/$formula_name:$version@sha256:$sha256"
-docker push "$sha256"
-
-# Commit and push changes
-cd "$(brew --prefix)"/Homebrew/Library/Taps/celsiusnarhwal/homebrew-htt || exit
+# Push formula changes
+cd "$(brew --prefix)"/Library/Taps/celsiusnarhwal/homebrew-htt || exit
 git pull --merge
 git push
